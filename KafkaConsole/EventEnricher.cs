c6102571d0892bf9ddd7339f0df97b5794734333
SJ <sjdev77@gmail.com>
2025-03-06 12:18:19 +0200

    Add Enricher

---
+++ KafkaConsole/EventEnricher.cs
@@ -0,0 +1,18 @@
+﻿using KafkaConsole.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KafkaConsole
+{
+    public class EventEnricher : IEventEnricher
+	{
+		public string EnrichEvent(string eventMessage)
+		{
+			// Enrich the event with simple logic (could be extended)
+			return $"{eventMessage} - Enriched";
+		}
+	}
+}
---
+++ KafkaConsole/KafkaEventConsumer.cs
@@ -0,0 +1,39 @@
+﻿using Confluent.Kafka;
+using KafkaConsole.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KafkaConsole
+{
+	public class KafkaEventConsumer : IEventConsumer
+	{
+		private readonly string _topic;
+		private readonly string _bootstrapServers;
+
+		public KafkaEventConsumer(string topic, string bootstrapServers)
+		{
+			_topic = topic;
+			_bootstrapServers = bootstrapServers;
+		}
+
+		public string ConsumeEvent()
+		{
+			var consumerConfig = new ConsumerConfig
+			{
+				GroupId = "test-consumer-group",
+				BootstrapServers = _bootstrapServers,
+				AutoOffsetReset = AutoOffsetReset.Earliest
+			};
+
+			using (var consumer = new ConsumerBuilder<Ignore, string>(consumerConfig).Build())
+			{
+				consumer.Subscribe(_topic);
+				var consumeResult = consumer.Consume();
+				return consumeResult.Message.Value;
+			}
+		}
+	}
+}
